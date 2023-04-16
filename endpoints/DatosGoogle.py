0a31a2a22ceac8b8d77261170151dd89c5f51369
Claudia Landeira Viñuela <clv1003@alu.ubu.es>
2023-04-16 18:31:36 +0200

    Implementado el inicio de sesión. Es posible acceder pero no se puede obtener la información de los chatbots externos al proyecto actual

--- endpoints/DatosGoogle.py
+++ endpoints/DatosGoogle.py
@@ -1,64 +1,41 @@
 # https://googleapis.dev/python/google-auth/1.7.0/reference/google.auth.transport.requests.html
-from google.auth.credentials import Credentials
-from google.oauth2 import id_token
-from google.auth.transport import requests
-from endpoints.datos import ProcesamientoDatosEndpoints
+import google.auth.exceptions
+import requests
+from google.oauth2.credentials import Credentials
+from google.auth.transport import requests as google_request
+from googleapiclient.discovery import build
 
-
-def googleLogin(token, client_id, project_id):
+'''
+def get_user_info(access_token):
     try:
-        info = id_token.verify_oauth2_token(token, requests.Request(), client_id)
+        idinfo = id_token.verify_oauth2_token(access_token, google_request.Request(), client_id)
 
-        # google_id = info['sub']
-        # google_email = info['email']
-        # google_name = info['name']
+        name = idinfo['name']
+        email = idinfo['email']
 
-        credenciales = Credentials.from_authorized_user_info(info)
-
-        chatbots = ProcesamientoDatosEndpoints.get_chatbots(project_id, credenciales)
-        return chatbots
-
-    except ValueError:
-        return "Invalid token"
-
-
+        return {'name': name, 'email': email}
+    except google.auth.exceptions.TransportError as e:
+        return 'Error de transporte: ' + str(e)
+    except ValueError as e:
+        return 'Error al verificar el token: ' + str(e)
 '''
-# -----------------------------------------------------------------------------------------------------
-# Obtener la url para cada request
-
-urlGeneral = 'https://dialogflow.googleapis.com/v2/projects/tfg-dialogflow-clv/agent'
 
 
-def obtenerURL(tipo):
-    if tipo == 1:  # Agente
-        return urlGeneral
+def get_user_info(token):
+    userinfo_endpoint = 'https://www.googleapis.com/oauth2/v3/userinfo'
+    headers = {
+        'Authorization': f'Bearer {token}'
+    }
 
-    if tipo == 2:  # Fulfillment
-        return urlGeneral + '/fulfillment'
-
-    if tipo == 3:  # Validation Result
-        return urlGeneral + '/validationResult'
-
-    if tipo == 4:  # entity types
-        return urlGeneral + '/entityTypes'
-
-    if tipo == 5:  # enviroments
-        return urlGeneral + '/enviroments'
-
-    return None
-
-
-# -----------------------------------------------------------------------------------------------------
-# Obtener el token de autenticacion para poder acceder y hacer peticiones
-
-def obtenerToken():
-    # Cargar las credenciales de la cuenta de servicio
-    credenciales = service_account.Credentials.from_service_account_file('json/client_secret.json')
-
-    # Obtener el token de acceso
-    auth_request = google.auth.transport.requests.Request()
-    credenciales.refresh(auth_request)
-    token_acceso = credenciales.token
-
-    return token_acceso
-'''
+    response = requests.get(userinfo_endpoint, headers=headers)
+    print(f'Response: {response.text}')
+    if response.status_code == 200:
+        userinfo_data = response.json()
+        user_info = {
+            'name': userinfo_data.get('name'),
+            'email': userinfo_data.get('email')
+        }
+        return user_info
+    else:
+        print(f'Error al obtener información del usuario: {response.status_code} - {response.text}')
+        return None
