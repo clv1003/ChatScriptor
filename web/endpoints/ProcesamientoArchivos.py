8ea4c0833951798268915957adaa61e5c425df04
Claudia Landeira Viñuela <clv1003@alu.ubu.es>
2023-04-30 20:15:39 +0200

    Cambiada la estructura de los archivos. Añadido el inicio de sesión y la db(csv)

---
+++ web/endpoints/ProcesamientoArchivos.py
@@ -0,0 +1,34 @@
+import os
+import re
+
+
+def get_disponible(rootdir):
+    chatbots = []
+
+    if os.path.exists(rootdir):
+        for file in os.listdir(rootdir):
+            chatbots.append(file)
+
+    return chatbots
+
+
+def get_arbol(rootdir):
+    arbol = {}
+
+    for subdir, dirs, files in os.walk(rootdir):
+        subdir_rel = os.path.relpath(subdir, rootdir)
+        aux_arbol = arbol
+
+        if subdir_rel != '.':
+            for d in subdir_rel.split(os.sep):
+                aux_arbol = aux_arbol.setdefault(d, {})
+
+        for file in files:
+            aux_arbol[file] = None
+
+    return arbol
+
+
+def relistado(cadena):
+    valores = re.findall(r"'(.*?)'", cadena)
+    return [valores[0], valores[1]]
