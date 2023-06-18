642dbb0e9ddb6fa27a41a64395bcddab0ad30af8
Claudia Landeira Viñuela <clv1003@alu.ubu.es>
2023-06-18 11:23:16 +0200

    Mejorado el traductor

--- web/endpoints/traductor/TraducirEntidades.py
+++ web/endpoints/traductor/TraducirEntidades.py
@@ -41,26 +41,27 @@ def traducirEntidades(traductor, rootdir, chat):
         entidades = ProcesamientoEntidadesIntents.get_entidades(rootdir + chat, traductor.getOriginal())
 
         for entidad in entidades:
-                dirs = ProcesamientoEntidadesIntents.directoriosEntidad(rootdir + chat, entidad[0],
-                                                                        traductor.getOriginal())
-                datos = ProcesamientoEntidadesIntents.get_json(dirs[0], dirs[1])
-                diccionarioValue = {}
-                for e in datos[1]:
-                    value = e["value"]
-                    diccionarioValue[value] = value
-                    #synonyms = eval(str(e["synonyms"]))
-                    # tr_synonyms = traductor.traducirFrase(synonyms)
-                    # ProcesamientoEntidadesIntents.editar_s_ent(rootdir + chat, synonyms, entidad[0], tr_synonyms,
-                    # traductor.getOriginal())
-                tr_value = traductor.traducirDiccionario(diccionarioValue)
+            dirs = ProcesamientoEntidadesIntents.directoriosEntidad(rootdir + chat, entidad[0],
+                                                                    traductor.getOriginal())
+            datos = ProcesamientoEntidadesIntents.get_json(dirs[0], dirs[1])
+            for e in datos[1]:
+                value = e["value"]
+                synonyms = e["synonyms"]
+                diccionarioSynonyms = {synonym: synonym for synonym in synonyms}
 
-                for clave, valor in tr_value.items():
-                    ProcesamientoEntidadesIntents.editar_v_ent(rootdir + chat, clave, entidad[0], valor,
-                                                               traductor.getOriginal())
-                print(f"\033[92m-> OK! - Traducido entidad {entidad[0]}\033[0m")
+                tr_value = traductor.traducirFrase(value)
+                tr_synonyms = traductor.traducirDiccionario(diccionarioSynonyms)
+                tr_synonyms = list(tr_synonyms.values())
 
-                if entidad[1].endswith('_entries_' + traductor.getOriginal() + '.json'):
-                    nombreNuevo = entidad[0].replace('.json', '') + '_entries_' + traductor.getIdioma() + '.json'
-                    rutaNueva = rootdir + chat + '/entities/' + nombreNuevo
-                    print(f' -> Directorio: {nombreNuevo} --> {rutaNueva}')
-                    os.rename(dirs[1], rutaNueva)
+                ProcesamientoEntidadesIntents.editar_v_ent(rootdir + chat, value, entidad[0], tr_value,
+                                                           traductor.getOriginal())
+                ProcesamientoEntidadesIntents.editar_s_ent(rootdir + chat, tr_value, entidad[0], tr_synonyms,
+                                                           traductor.getOriginal())
+
+            print(f"\033[92m-> OK! - Traducido entidad {entidad[0]}\033[0m")
+
+            if entidad[1].endswith('_entries_' + traductor.getOriginal() + '.json'):
+                nombreNuevo = entidad[0].replace('.json', '') + '_entries_' + traductor.getIdioma() + '.json'
+                rutaNueva = rootdir + chat + '/entities/' + nombreNuevo
+                print(f' -> Directorio: {nombreNuevo} --> {rutaNueva}')
+                os.rename(dirs[1], rutaNueva)
