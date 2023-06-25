bcfb43d77fd47704cef08fa0e81b8d0588ff8344
Claudia Landeira Viñuela <clv1003@alu.ubu.es>
2023-06-25 11:21:29 +0200

    Correcciones

--- web/endpoints/traductor/TraducirEntidades.py
+++ web/endpoints/traductor/TraducirEntidades.py
@@ -1,38 +1,6 @@
 import os.path
 import ProcesamientoEntidadesIntents
 
-'''
-def traducirEntidades(traductor, rootdir, chat, original, idioma):
-    if os.path.exists(rootdir + chat + '/entities'):
-        print(f'')
-        print("\033[91m ---------------------------- ENTIDADES \033[0m")
-        print(f'{rootdir + chat}')
-        entidades = ProcesamientoEntidadesIntents.get_entidades(rootdir + chat, original)
-        print(f'{entidades}')
-        print(f'')
-
-        for entidad in entidades:
-            dirs = ProcesamientoEntidadesIntents.directoriosEntidad(rootdir + chat, entidad[0], original)
-            datos = ProcesamientoEntidadesIntents.get_json(dirs[0], dirs[1])
-
-            for e in datos[1]:
-                value = e["value"]
-                synonyms = eval(str(e["synonyms"]))
-                print(f'{synonyms}')
-                tr_value = traductor.traducir(value, original, idioma)
-                tr_synonyms = traductor.traducir(synonyms, original, idioma)
-                print(f'{tr_value} - {tr_synonyms}')
-                ProcesamientoEntidadesIntents.editar_v_ent(rootdir + chat, value, entidad[0], tr_value, original)
-                ProcesamientoEntidadesIntents.editar_s_ent(rootdir + chat, synonyms, entidad[0], tr_synonyms, original)
-                print("\033[92m -> OK! - Modificado value y sinonimos \033[0m")
-            if entidad[1].endswith('_entries_' + original + '.json'):
-                nombreNuevo = entidad[0].replace('.json', '') + '_entries_' + idioma + '.json'
-                rutaNueva = rootdir + chat + '/entities/' + nombreNuevo
-                print(f'Cambio de directorio: {nombreNuevo} --> {rutaNueva}')
-                print(f'')
-                os.rename(dirs[1], rutaNueva)
-'''
-
 
 def traducirEntidades(traductor, rootdir, chat):
     if os.path.exists(rootdir + chat + '/entities'):
@@ -44,6 +12,10 @@ def traducirEntidades(traductor, rootdir, chat):
             dirs = ProcesamientoEntidadesIntents.directoriosEntidad(rootdir + chat, entidad[0],
                                                                     traductor.getOriginal())
             datos = ProcesamientoEntidadesIntents.get_json(dirs[0], dirs[1])
+
+            name = datos[0]['name']
+            tr_name = traductor.traducirFrase(name)
+
             for e in datos[1]:
                 value = e["value"]
                 synonyms = e["synonyms"]
@@ -57,6 +29,8 @@ def traducirEntidades(traductor, rootdir, chat):
                                                            traductor.getOriginal())
                 ProcesamientoEntidadesIntents.editar_s_ent(rootdir + chat, tr_value, entidad[0], tr_synonyms,
                                                            traductor.getOriginal())
+
+            ProcesamientoEntidadesIntents.editar_nombre(dirs[0], 'name', tr_name)
 
             print(f"\033[92m-> OK! - Traducido entidad {entidad[0]}\033[0m")
 
--- web/endpoints/traductor/TraducirIntents.py
+++ web/endpoints/traductor/TraducirIntents.py
@@ -1,59 +1,5 @@
 import os.path
-
-from unidecode import unidecode
-
 import ProcesamientoEntidadesIntents
-
-'''
-def traducirIntents(traductor, rootdir, chat, original, idioma):
-    if os.path.exists(rootdir + chat + '/intents'):
-        print(f'')
-        print("\033[91m ---------------------------- INTENTS \033[0m")
-        print(f'{rootdir + chat}')
-        intents = ProcesamientoEntidadesIntents.get_intents(rootdir + chat, original)
-        print(f'{intents}')
-        print(f'')
-
-        for intent in intents:
-            dirs = ProcesamientoEntidadesIntents.directoriosIntent(rootdir + chat, intent[0], original)
-            datos = ProcesamientoEntidadesIntents.get_json(dirs[0], dirs[1])
-
-            if 'speech' in datos[0]['responses'][0]['messages'][0]:
-                speech = datos[0]['responses'][0]['messages'][0]['speech']
-
-                for s in speech:
-                    tr_speech = traductor.traducir(s, original, idioma)
-                    print(f'{tr_speech}')
-                    ProcesamientoEntidadesIntents.editar_messages(dirs[0], s, tr_speech)
-                print("\033[92m -> OK! - Modificado Speech \033[0m")
-
-            for i in datos[1]:
-                for j in i['data']:
-                    if len(j) == 2:
-                        text = j['text']
-                        if len(text) > 0:
-                            tr_text = traductor.traducir(text, original, idioma)
-                            print(f'{tr_text}')
-                            ProcesamientoEntidadesIntents.editar_data(rootdir+chat, intent[0], text, 'text', tr_text
-                                                                      , original)
-                            print("\033[92m -> OK! - Modificado text 1 \033[0m")
-                    elif j['meta']:
-                        text = j['text']
-                        if len(text) > 0:
-                            tr_text = traductor.traducir(text, original, idioma)
-
-                            print(f'{tr_text}')
-                            ProcesamientoEntidadesIntents.editar_data(rootdir+chat, intent[0], text, 'text', tr_text
-                                                                      , original)
-                            print("\033[92m -> OK! - Modificado text 2 \033[0m")
-
-            if intent[1].endswith('_usersays_' + original + '.json'):
-                nombreNuevo = intent[0].replace('.json', '') + '_usersays_' + idioma + '.json'
-                rutaNueva = rootdir + chat + '/intents/' + nombreNuevo
-                print(f'Cambio de directorio: {nombreNuevo} --> {rutaNueva}')
-                print(f'')
-                os.rename(dirs[1], rutaNueva)
-'''
 
 
 def traducirIntents(traductor, rootdir, chat):
